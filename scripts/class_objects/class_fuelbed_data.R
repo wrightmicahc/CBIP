f4be9764dd9d98e53838772c0c2cc01004596787
Micah Wright <micahwright@Micahs-MacBook-Pro.local>
2018-08-15 11:28:12 -0700

    Initial commi class_fuelbed_data

---
+++ scripts/class_objects/class_fuelbed_data.R
@@ -0,0 +1,35 @@
+################################################################################
+# This script creates an object class that stores the data necessary to run the
+# consume model.
+#
+# slots:
+# xy_coords: two column matrix with raster cell lat-long  
+# fuelbed_number: FCCS fuelbed ID number
+# fuel_load: FCCS fuel loading data with columns for fuel size class
+# residue: harvest and treatment residues
+# treatmet: silvaculture treatment 
+# fuel moisture: data frame with columns for  97.5 percentile values for 10 and 
+# 1,000-hr fuel moisture
+# slope: % slope
+# wind: uncorrected windspeed
+# tpi: normalized terrain prominance index
+# pulp_market: is pulp market present?
+# days_since_rain: the number of days since 0.25" rainfall
+#
+# author: Micah Wright
+# 
+################################################################################
+setClass("fuelbed_data",
+         slots = c(xy_coords = "matrix",
+                   fuelbed_number = "integer",
+                   FCID2018 = "integer",
+                   fuel_load = "data.frame",
+                   residue = "data.frame",
+                   treatment = "character",
+                   fuel_moisture = "data.frame",
+                   slope = "numeric",
+                   wind = "numeric",
+                   tpi = "numeric",
+                   pulp_market = "logical",
+                   days_since_rain = "integer")
+)
