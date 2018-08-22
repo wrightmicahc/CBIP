43daa2dff4fe200b0acc82b6020f0acaf561d289
Micah Wright <micahwright@Micahs-MacBook-Pro.local>
2018-08-22 13:01:47 -0700

    Remove fuelload subclass.

--- scripts/class_objects/class_fuelbed_data.R
+++ scripts/class_objects/class_fuelbed_data.R
@@ -3,12 +3,12 @@
 # consume model.
 #
 # slots:
-# xy_coords: two column matrix with raster cell lat-long  
+# xy_coords: two column matrix with raster cell lat-long
 # fuelbed_number: FCCS fuelbed ID number
 # fuel_load: FCCS fuel loading data with columns for fuel size class
 # residue: harvest and treatment residues
-# treatmet: silvaculture treatment 
-# fuel moisture: data frame with columns for  97.5 percentile values for 10 and 
+# treatmet: silvaculture treatment
+# fuel moisture: data frame with columns for  97.5 percentile values for 10 and
 # 1,000-hr fuel moisture
 # slope: % slope
 # wind: uncorrected windspeed
@@ -17,22 +17,56 @@
 # days_since_rain: the number of days since 0.25" rainfall
 #
 # author: Micah Wright
-# 
+#
 ################################################################################
-setClass("fuelbed_data",
-         
-         slots = c(xy_coords = "matrix",
-                   fuelbed_number = "integer",
-                   fcid2018 = "integer",
-                   fuel_load = "data.frame",
-                   residue = "data.frame",
-                   treatment = "character",
-                   fm1000 = "numeric",
-                   fm10 = "numeric",
-                   slope = "numeric",
-                   wind = "numeric",
-                   tpi = "numeric",
-                   pulp_market = "logical",
-                   days_since_rain = "integer",
-                   fm_type = "character")
+
+.Fuelbed <- setClass("Fuelbed",
+                     #contains = "FuelLoad",
+                     slots = c(xy_coords = "matrix",
+                               fuelbed_number = "integer",
+                               fcid2018 = "integer",
+                               fuel_load = "data.frame",
+                               residue = "data.frame",
+                               treatment = "character",
+                               fm1000 = "numeric",
+                               fm10 = "numeric",
+                               slope = "numeric",
+                               wind = "numeric",
+                               tpi = "numeric",
+                               pulp_market = "logical",
+                               days_since_rain = "integer",
+                               fm_type = "character")
 )
+
+Fuelbed <- function(xy_coords,
+                    fuelbed_number,
+                    fcid2018, 
+                    fuelbed, 
+                    residue,
+                    treatment,
+                    fm1000, 
+                    fm10,
+                    slope, 
+                    wind, 
+                    tpi,
+                    pulp_market,
+                    days_since_rain,
+                    fm_type) {
+        
+        .Fuelbed(xy_coords = xy_coords,
+                 fuelbed_number = fuelbed_number,
+                 fcid2018 = fcid2018,
+                 treatment = treatment,
+                 fuel_load = fuelbed[fuelbed$fuelbed_number == fuelbed_number, ],
+                 residue = residue[residue$FCID2018 == fcid2018 &
+                                           residue$Treatment == treatment, ],
+                 fm1000 = fm1000,
+                 fm10 = fm10,
+                 slope = slope,
+                 wind = wind,
+                 tpi = tpi,
+                 pulp_market = pulp_market,
+                 days_since_rain = days_since_rain,
+                 fm_type = fm_type)
+}
+
