50a4f4425a19076e7ec9d637181534b1fee32f0e
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-11-13 14:58:38 -0700

    Remove snag data, as it doesn't have the correct columns. Include code for no action.

--- scripts/Other/break_residue.R
+++ scripts/Other/break_residue.R
@@ -27,7 +27,6 @@ break_residue <- function(treatment, harvest_type, harvest_system) {
         
         file_paths <- list("No_Action" = "data/UW/batch_out/Treatment_NoAction.dbf",
                            "Clearcut" = "data/UW/batch_out/Treatment_Remove100Percent.dbf",
-                           "Standing_Dead" = "data/UW/batch_out/Treatment_Snags.dbf",
                            "Thin_Above_20" = "data/UW/batch_out/Treatment_ThinFromAboveRemove20PercentBA.dbf",
                            "Thin_Above_40" = "data/UW/batch_out/Treatment_ThinFromAboveRemove40PercentBA.dbf",
                            "Thin_Above_60" = "data/UW/batch_out/Treatment_ThinFromAboveRemove60PercentBA.dbf",
@@ -41,56 +40,82 @@ break_residue <- function(treatment, harvest_type, harvest_system) {
                            "Thin_Proportional_60" = "data/UW/batch_out/Treatment_ThinProportionalRemove60PercentBA.dbf",
                            "Thin_Proportional_80" = "data/UW/batch_out/Treatment_ThinProportionalRemove80PercentBA.dbf")
         
-        if(harvest_type == "Whole_Tree" & harvest_system == "Ground") {
-                breakage <- 0.14
+        if(treatment == "No_Action") {
+   
+                # load the file
+                residue <- read.dbf(file_paths[[treatment]], as.is = TRUE)
+                residue <- as.data.table(residue)
+                residue[, Treatment := treatment]
+                
+                # update 
+                broken_residue <- residue[, .(FCID2018 = Value,
+                                              Treatment = Treatment,
+                                              Harvest_type = harvest_type, 
+                                              Harvest_system = harvest_system,
+                                              TPA = TPA,
+                                              Pulp_6t9_tonsAcre = 0.0,
+                                              Break_6t9_tonsAcre = 0.0,
+                                              Pulp_4t6_tonsAcre = 0.0,
+                                              Break_4t6_tonsAcre = 0.0,
+                                              Break_ge9_tonsAcre = 0.0,
+                                              Branch_tonsAcre = 0.0,
+                                              Foliage_tonsAcre = 0.0)]
         }
         
-        if(harvest_type == "Whole_Tree" & harvest_system == "Cable") {
-                breakage <- 0.17
+        if(treatment != "No_Action") {
+                if(harvest_type == "Whole_Tree" & harvest_system == "Ground") {
+                        breakage <- 0.14
+                }
+                
+                if(harvest_type == "Whole_Tree" & harvest_system == "Cable") {
+                        breakage <- 0.17
+                }
+                
+                if(harvest_type == "CTL" & harvest_system %in% c("Cable", "Ground")) {
+                        breakage <- 0.10
+                }
+                
+                
+                # load the file
+                residue <- read.dbf(file_paths[[treatment]], as.is = TRUE)
+                residue <- as.data.table(residue)
+                residue[, Treatment := treatment]
+                
+                # update 
+                broken_residue <- residue[, .(FCID2018 = Value,
+                                              Treatment = Treatment,
+                                              Harvest_type = harvest_type, 
+                                              Harvest_system = harvest_system,
+                                              TPA = TPA,
+                                              Pulp_6t9_tonsAcre = sum_residue(dt = residue,
+                                                                              columns = c("CutStem6BL",
+                                                                                          "CutBarkSte"),
+                                                                              breakage = breakage,
+                                                                              is_pulp = TRUE),
+                                              Break_6t9_tonsAcre = sum_residue(dt = residue,
+                                                                               columns = c("CutStem6BL",
+                                                                                           "CutBarkSte"),
+                                                                               breakage = breakage,
+                                                                               is_pulp = FALSE),
+                                              Pulp_4t6_tonsAcre = sum_residue(dt = residue,
+                                                                              columns = c("CutStem4To",
+                                                                                          "CutBarkS_2"),
+                                                                              breakage = breakage,
+                                                                              is_pulp = TRUE),
+                                              Break_4t6_tonsAcre = sum_residue(dt = residue,
+                                                                               columns = c("CutStem4To",
+                                                                                           "CutBarkS_2"),
+                                                                               breakage = breakage,
+                                                                               is_pulp = FALSE),
+                                              Break_ge9_tonsAcre = sum_residue(dt = residue,
+                                                                               columns = c("CutStem6BG",
+                                                                                           "CutBarkS_1"),
+                                                                               breakage = breakage,
+                                                                               is_pulp = FALSE),
+                                              Branch_tonsAcre = CutBranchB/2000,
+                                              Foliage_tonsAcre = CutFoliage/2000)]
         }
         
-        if(harvest_type == "CTL" & harvest_system %in% c("Cable", "Ground")) {
-                breakage <- 0.10
-        }
-        
-        # load the file
-        residue <- read.dbf(file_paths[[treatment]], as.is = TRUE)
-        residue <- as.data.table(residue)
-        residue[, Treatment := treatment]
-        
-        # update 
-        broken_residue <- residue[, .(FCID2018 = Value,
-                                      Treatment = Treatment,
-                                      Harvest_type = harvest_type, 
-                                      Harvest_system = harvest_system,
-                                      TPA = TPA,
-                                      Pulp_6t9_tonsAcre = sum_residue(dt = residue,
-                                                                      columns = c("CutStem6BL",
-                                                                                  "CutBarkSte"),
-                                                                      breakage = breakage,
-                                                                      is_pulp = TRUE),
-                                      Break_6t9_tonsAcre = sum_residue(dt = residue,
-                                                                       columns = c("CutStem6BL",
-                                                                                   "CutBarkSte"),
-                                                                       breakage = breakage,
-                                                                       is_pulp = FALSE),
-                                      Pulp_4t6_tonsAcre = sum_residue(dt = residue,
-                                                                      columns = c("CutStem4To",
-                                                                                  "CutBarkS_2"),
-                                                                      breakage = breakage,
-                                                                      is_pulp = TRUE),
-                                      Break_4t6_tonsAcre = sum_residue(dt = residue,
-                                                                       columns = c("CutStem4To",
-                                                                                   "CutBarkS_2"),
-                                                                       breakage = breakage,
-                                                                       is_pulp = FALSE),
-                                      Break_ge9_tonsAcre = sum_residue(dt = residue,
-                                                                       columns = c("CutStem6BG",
-                                                                                   "CutBarkS_1"),
-                                                                       breakage = breakage,
-                                                                       is_pulp = FALSE),
-                                      Branch_tonsAcre = CutBranchB/2000,
-                                      Foliage_tonsAcre = CutFoliage/2000)]
         return(broken_residue)
 }
         
