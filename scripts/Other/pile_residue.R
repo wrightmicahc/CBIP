1fa0b2af93108ec668ad4798e4ec36b8808cfaa8
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-11-14 19:01:53 -0700

    Huge change to pile function, now ifelse based on harvest type and system.

--- scripts/Other/pile_residue.R
+++ scripts/Other/pile_residue.R
@@ -1,10 +1,15 @@
 ################################################################################
-# This script adds a column to specifie a pile proportion for biomass residue as
+# This script adds a column to specify a pile proportion for biomass residue as
 # part of the California Biopower Impact Project. 
 #
 # Author: Micah Wright, Humboldt State University
 ################################################################################
 
-pile_residue <- function(dt, piled) {
-        dt[, piled_prop := piled]
+pile_residue <- function(h_type, h_system) {
+        browser()
+        
+        piled <- ifelse(h_type == "Whole_Tree" & h_system == "Ground", 0.7,
+                        ifelse(h_type == "Whole_Tree" & h_system == "Cable", 0.6, 0.0))  
+        
+        return(piled)
 }
