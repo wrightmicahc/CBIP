fcf78b08b10b79ba526678b37c8ec45923de25b3
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-11-17 16:08:29 -0700

    Once again, completely refactor pile residue function. Create inout for dt and pulp market.

--- scripts/Other/pile_residue.R
+++ scripts/Other/pile_residue.R
@@ -5,11 +5,26 @@
 # Author: Micah Wright, Humboldt State University
 ################################################################################
 
-pile_residue <- function(h_type, h_system) {
-        browser()
+pile_residue <- function(dt, pm) {
         
-        piled <- ifelse(h_type == "Whole_Tree" & h_system == "Ground", 0.7,
-                        ifelse(h_type == "Whole_Tree" & h_system == "Cable", 0.6, 0.0))  
+        pile_prop <- function(tp, syst) {
+                ifelse(tp == "Whole_Tree" & syst == "Ground", 0.7,
+                       ifelse(tp == "Whole_Tree" & syst == "Cable", 0.6, 0.0)) 
+        }
         
-        return(piled)
+        if(pm == "present") {
+                dt[, pile_load := (Break_4t6_tonsAcre + Break_6t9_tonsAcre) * pile_prop(Harvest_type,
+                                                                         Harvest_system)]
+        }
+        
+        if(pm == "absent") {
+                dt[, pile_load := (Break_4t6_tonsAcre +
+                                           Break_6t9_tonsAcre +
+                                           Pulp_4t6_tonsAcre +
+                                           Pulp_6t9_tonsAcre) * 
+                           pile_prop(Harvest_type,
+                                     Harvest_system)]
+        }
+        
+        return(dt)
 }
