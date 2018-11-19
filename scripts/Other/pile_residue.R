de6009a4a7000debe742657b38033b678917dc4a
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-11-18 19:06:37 -0700

    Many changee. Add other columns to pile, including foliage. Change pile column name.

--- scripts/Other/pile_residue.R
+++ scripts/Other/pile_residue.R
@@ -1,6 +1,8 @@
 ################################################################################
 # This script adds a column to specify a pile proportion for biomass residue as
 # part of the California Biopower Impact Project. 
+# 
+# Currently, it is assumed that foliage is piled along with the branches
 #
 # Author: Micah Wright, Humboldt State University
 ################################################################################
@@ -8,22 +10,37 @@
 pile_residue <- function(dt, pm) {
         
         pile_prop <- function(tp, syst) {
-                ifelse(tp == "Whole_Tree" & syst == "Ground", 0.7,
-                       ifelse(tp == "Whole_Tree" & syst == "Cable", 0.6, 0.0)) 
+                gets_piled <- ifelse(tp == "Whole_Tree" & syst == "Ground", 
+                                     0.7,
+                                     ifelse(tp == "Whole_Tree" & syst == "Cable",
+                                            0.6, 0.0)) 
+                return(gets_piled)
         }
         
         if(pm == "present") {
-                dt[, pile_load := (Break_4t6_tonsAcre + Break_6t9_tonsAcre) * pile_prop(Harvest_type,
-                                                                         Harvest_system)]
+                
+                dt[, pile_landing := (Break_4t6_tonsAcre +
+                                           Break_6t9_tonsAcre +
+                                           Branch_tonsAcre +
+                                           Break_ge9_tonsAcre +
+                                           Foliage_tonsAcre) *
+                           pile_prop(Harvest_type,
+                                     Harvest_system)]
+                
         }
         
         if(pm == "absent") {
-                dt[, pile_load := (Break_4t6_tonsAcre +
+                
+                dt[, pile_landing := (Break_4t6_tonsAcre +
                                            Break_6t9_tonsAcre +
+                                           Branch_tonsAcre +
+                                           Break_ge9_tonsAcre +
                                            Pulp_4t6_tonsAcre +
-                                           Pulp_6t9_tonsAcre) * 
+                                           Pulp_6t9_tonsAcre +
+                                           Foliage_tonsAcre) *
                            pile_prop(Harvest_type,
                                      Harvest_system)]
+                
         }
         
         return(dt)
