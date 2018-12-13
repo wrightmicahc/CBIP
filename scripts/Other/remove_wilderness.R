d0d14f8d6df05f7fba677366b4ea8312b574b298
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-12-13 11:30:35 -0800

    Initial commit.

---
+++ scripts/Other/remove_wilderness.R
@@ -0,0 +1,23 @@
+library(sf)
+library(tidyverse)
+
+# load tiles
+tiles <- st_read("data/Tiles/good_tiles.shp", quiet = TRUE)
+
+# load CA wilderness areas, taken from figshare
+wilderness <- st_read("data/Other/wilderness/Wilderness_Areas_CA.shp", quiet = TRUE)
+
+# reproject wilderness
+wilderness <- st_transform(wilderness,
+                             st_crs(tiles))
+
+# select wilderness tiles
+wild_tiles <- tiles[wilderness, ]
+
+# get all tiles not in the wilderness
+tiles_clip <- filter(tiles, !(ID %in% wild_tiles$ID))
+
+# save to a file
+st_write(tiles_clip, 
+         "data/Tiles/clipped_tiles",
+         driver = "ESRI Shapefile")
