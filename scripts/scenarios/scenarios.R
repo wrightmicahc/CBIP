007f799cc52e79440c66f266ad4e5179f5875158
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2018-12-19 13:02:36 -0800

    Parallelize emissions calculations process.

--- scripts/scenarios/scenarios.R
+++ scripts/scenarios/scenarios.R
@@ -43,42 +43,44 @@ residue_scenario <- function(tile_number) {
         
         # load data
         # this combines residue, raster, and FCCS data
-        emissions_list <- lapply(scenario_list, function(x) {
-                
-                ID <- x[1, ID]
-                Silvicultural_Treatment <- x[1, Silvicultural_Treatment]
-                Harvest_System <- x[1, Harvest_System]
-                Harvest_Type <- x[1, Harvest_Type]
-                Burn_Type <- x[1, Burn_Type]
-                Biomass_Collection <- x[1, Biomass_Collection]
-                Tile_Number <- x[1, Tile_Number]
-                
-                fuel_df <- load_data(ID,
-                                     Silvicultural_Treatment,
-                                     Harvest_System,
-                                     Harvest_Type, 
-                                     Burn_Type,
-                                     Biomass_Collection,
-                                     Tile_Number)
-                
-                # correct windspeed
-                wind_correction(fuel_df,
-                                Wind,
-                                TPA,
-                                TPI)
-                
-                # calculate piled load
-                fuel_df <- pile_residue(fuel_df)
-                
-                # add the remaining residue to the fuelbed
-                fuel_df <-  add_residue(fuel_df)
-                
-                # calculate emissions
-                output <- try(burn_residue(fuel_df, Burn_Type))
-                
-                return(output)
-                
-        })
+        emissions_list <- mclapply(scenario_list, 
+                                   mc.cores = detectCores(),
+                                   function(x) {
+                                           
+                                           ID <- x[1, ID]
+                                           Silvicultural_Treatment <- x[1, Silvicultural_Treatment]
+                                           Harvest_System <- x[1, Harvest_System]
+                                           Harvest_Type <- x[1, Harvest_Type]
+                                           Burn_Type <- x[1, Burn_Type]
+                                           Biomass_Collection <- x[1, Biomass_Collection]
+                                           Tile_Number <- x[1, Tile_Number]
+                                           
+                                           fuel_df <- load_data(ID,
+                                                                Silvicultural_Treatment,
+                                                                Harvest_System,
+                                                                Harvest_Type, 
+                                                                Burn_Type,
+                                                                Biomass_Collection,
+                                                                Tile_Number)
+                                           
+                                           # correct windspeed
+                                           wind_correction(fuel_df,
+                                                           Wind,
+                                                           TPA,
+                                                           TPI)
+                                           
+                                           # calculate piled load
+                                           fuel_df <- pile_residue(fuel_df)
+                                           
+                                           # add the remaining residue to the fuelbed
+                                           fuel_df <-  add_residue(fuel_df)
+                                           
+                                           # calculate emissions
+                                           output <- try(burn_residue(fuel_df, Burn_Type))
+                                           
+                                           return(output)
+                                           
+                                   })
         
         emissions_df <- rbindlist(emissions_list)
         
