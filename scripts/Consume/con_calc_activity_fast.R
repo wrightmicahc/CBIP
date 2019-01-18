16e2e2e81439bf038517ce6566dfa13e446aa730
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2019-01-17 16:30:06 -0800

    Create output for residual (unburned) fuels and return with emissions as named output list.

--- scripts/Consume/con_calc_activity_fast.R
+++ scripts/Consume/con_calc_activity_fast.R
@@ -342,6 +342,38 @@ ccon_activity_fast <- function(dt, fm_type, days_since_rain, DRR){
                  resid_pile_landing = (pile_landing * 0.9) * 0.15)]
         
         ###################################################
+        # Calculate residual fuels (those not consumed)
+        # we need to export a new data table with just the export data
+        ###################################################
+        dt_resid <- dt[, .(x, 
+                           y,
+                           fuelbed_number,
+                           FCID2018,
+                           ID, 
+                           Silvicultural_Treatment,
+                           Harvest_Type,
+                           Harvest_System,
+                           Burn_Type,
+                           Biomass_Collection,
+                           Slope,
+                           Fm10,
+                           Fm1000,
+                           Wind_corrected,
+                           duff_upper_loading = duff_upper_loading - (flamg_duff + smoldg_duff + resid_duff),
+                           litter_loading = litter_loading - (flamg_litter + smoldg_litter + resid_litter), 
+                           one_hr_sound = one_hr_sound - (flamg_1 + smoldg_1 + resid_1), 
+                           ten_hr_sound = ten_hr_sound - (flamg_10 + smoldg_10 + resid_10),
+                           hun_hr_sound = hun_hr_sound - (flamg_100 + smoldg_100 + resid_100),
+                           oneK_hr_sound = oneK_hr_sound - (flamg_OneK_snd + smoldg_OneK_snd + resid_OneK_snd),
+                           oneK_hr_rotten = oneK_hr_rotten - (flamg_OneK_rot + smoldg_OneK_rot + resid_OneK_rot),
+                           tenK_hr_sound = tenK_hr_sound - (flamg_tenK_snd + smoldg_tenK_snd + resid_tenK_snd), 
+                           tenK_hr_rotten = tenK_hr_rotten - (flamg_tenK_rot + smoldg_tenK_rot + resid_tenK_rot),
+                           tnkp_hr_sound = tnkp_hr_sound - (flamg_tnkp_snd + smoldg_tnkp_snd + resid_tnkp_snd),
+                           tnkp_hr_rotten = tnkp_hr_rotten - (flamg_tnkp_rot + smoldg_tnkp_rot + resid_tnkp_rot),
+                           pile_field = pile_field - (flamg_pile_field + smoldg_pile_field + resid_pile_field),
+                           pile_landing = pile_landing - (flamg_pile_landing + smoldg_pile_landing + resid_pile_landing))]
+        
+        ###################################################
         # Calculate emissions
         # #FORMERLY emiss_calc()
         # we need to export a new data table with just the export data
@@ -551,6 +583,6 @@ ccon_activity_piled_only_fast <- function(dt){
                            smoldering_VOC, residual_CH4, residual_CO, residual_CO2, residual_NH3, residual_NOx, residual_PM10, residual_PM2.5, residual_SO2, residual_VOC, total_CH4, total_CO, total_CO2, total_NH3,
                            total_NOx, total_PM10, total_PM2.5, total_SO2, total_VOC)]
         
-        return(out_dt)
+        return(list("out_dt" = out_dt, "dt_resid" = dt_resid))
 }
         
