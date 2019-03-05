d8855585eb72bee408169c7c850ba7e8554fd22d
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2019-03-05 12:39:12 -0800

    Add an option to call extreme or average fire weather.

--- scripts/scenarios/get_raster_list.R
+++ scripts/scenarios/get_raster_list.R
@@ -4,21 +4,32 @@
 # Author: Micah Wright, Humboldt State University
 ################################################################################
 
-get_raster_list <- function(scenario) {
+get_raster_list <- function(scenario, conditions) {
         
         stopifnot(scenario %in% c("None", 
                                   "Pile", 
                                   "Broadcast", 
                                   "Jackpot"))
         
-        if(scenario == "None") 
+        if(scenario == "None" & conditions == "Average") 
         {
                 files <- list("FCID2018" = "data/UW/FCID2018_masked.tif",
                               "Slope" = "data/Other/DEM/Slope_NAD83.tif",
                               "fuelbed_number" = "data/FCCS/spatial/FCCS_NAD83.tif", 
-                              "Fm10" = "data/GEE/resampled/fm10.tif",
-                              "Fm1000" = "data/GEE/resampled/fm1000.tif",
-                              "Wind" = "data/GEE/resampled/windv.tif",
+                              "Fm10" = "data/GEE/resampled/fm10_50.tif",
+                              "Fm1000" = "data/GEE/resampled/fm1000_50.tif",
+                              "Wind" = "data/GEE/resampled/windv_50.tif",
+                              "TPI" = "data/Other/DEM/dem_dev_2g_NAD83.tif")
+        }
+        
+        if(scenario == "None" & conditions == "Extreme") 
+        {
+                files <- list("FCID2018" = "data/UW/FCID2018_masked.tif",
+                              "Slope" = "data/Other/DEM/Slope_NAD83.tif",
+                              "fuelbed_number" = "data/FCCS/spatial/FCCS_NAD83.tif", 
+                              "Fm10" = "data/GEE/resampled/fm10_97.tif",
+                              "Fm1000" = "data/GEE/resampled/fm1000_97.tif",
+                              "Wind" = "data/GEE/resampled/windv_97.tif",
                               "TPI" = "data/Other/DEM/dem_dev_2g_NAD83.tif")
         }
         
@@ -27,9 +38,9 @@ get_raster_list <- function(scenario) {
                 files <- list("FCID2018" = "data/UW/FCID2018_masked.tif",
                               "Slope" = "data/Other/DEM/Slope_NAD83.tif",
                               "fuelbed_number" = "data/FCCS/spatial/FCCS_NAD83.tif", 
-                              "Fm10" = "data/GEE/resampled/fm10_rx.tif",
-                              "Fm1000" = "data/GEE/resampled/fm1000_rx.tif",
-                              "Wind" = "data/GEE/resampled/windv_rx.tif",
+                              "Fm10" = "data/GEE/resampled/fm10_375.tif",
+                              "Fm1000" = "data/GEE/resampled/fm1000_375.tif",
+                              "Wind" = "data/GEE/resampled/windv_375.tif",
                               "TPI" = "data/Other/DEM/dem_dev_2g_NAD83.tif")
         }
         
