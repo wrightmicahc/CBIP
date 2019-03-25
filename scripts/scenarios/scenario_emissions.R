cdd8cc0e034aa6a7ed362223e2c888e4bfe5555b
Micah C Wright <mcw11@SERC-S26492.local>
2019-03-25 13:35:31 -0700

    Use future.apply for parrallel, it's not platform specific.

--- scripts/scenarios/scenario_emissions.R
+++ scripts/scenarios/scenario_emissions.R
@@ -32,11 +32,13 @@ source("scripts/Other/add_rx_residue.R")
 # source function for adding rx residues back to recovered fuelbed
 source("scripts/Other/save_output.R")
 
-library(parallel)
+library(future.apply)
+
+plan(multiprocess)
 
 scenario_emissions <- function(tile_number) {
         
-        # create output tile folders if missiong
+        # create output tile folders if missing
         em_path <- "data/Tiles/output/emissions/"
         res_path <- "data/Tiles/output/residual_fuels/"
         
@@ -61,137 +63,136 @@ scenario_emissions <- function(tile_number) {
         scenario_list <- split(scenarios, by = "ID")
         
         # decay the fuels over 100 years and save the output
-        mclapply(scenario_list, 
-                 mc.cores = detectCores(),
-                 function(x) {
-                         
-                         ID <- x[1, ID]
-                         Silvicultural_Treatment <- x[1, Silvicultural_Treatment]
-                         Harvest_System <- x[1, Harvest_System]
-                         Harvest_Type <- x[1, Harvest_Type]
-                         Burn_Type <- x[1, Burn_Type]
-                         Biomass_Collection <- x[1, Biomass_Collection]
-                         Tile_Number <- x[1, Tile_Number]
-                         
-                         # load data
-                         # this combines residue, raster, and FCCS data
-                         fuel_df <- load_data(ID,
-                                              Silvicultural_Treatment,
-                                              Harvest_System,
-                                              Harvest_Type, 
-                                              Burn_Type,
-                                              Biomass_Collection,
-                                              Tile_Number)
-                         
-                         # correct windspeed
-                         wind_correction(fuel_df,
-                                         Wind,
-                                         TPA,
-                                         TPI)
-                         
-                         if(Burn_Type != "None") {
-                                 
-                                 # need to copy dt or it is modified 
-                                 cpy <- copy(fuel_df)
-                                 
-                                 # calculate piled load
-                                 cpy <- pile_residue(cpy, 0)
-                                 
-                                 # add the remaining residue to the fuelbed
-                                 cpy <-  add_residue(cpy, 0)
-                                 
-                                 # change fire weather value names appropriately
-                                 cpy[, ':=' (Wind_corrected = Wind_corrected_rx,
-                                             Fm10  = Fm10_rx,
-                                             Fm1000 = Fm1000_rx)]
-                                 
-                                 # apply the rx burn
-                                 rx_out <- burn_residue(cpy, Burn_Type)
-                                 
-                                 # save the output
-                                 save_output(rx_out,
-                                             Silvicultural_Treatment,
-                                             Harvest_System,
-                                             Harvest_Type,
-                                             Burn_Type,
-                                             tile_number,
-                                             0)
-                                 
-                                 # create a vector from 25-100 years
-                                 timestep <- seq(25, 100, 25)
-                                 
-                                 # assign the vector names, otherwise position will be 
-                                 # off by 1 from the value
-                                 names(timestep) <- as.character(timestep)
-                                 
-                                 # calculate the remaining fuel for each timestep and
-                                 # add to the fuelbed
-                                 lapply(timestep, function(i) {
-                                         
-                                         # need to copy dt or it is modified 
-                                         post_rx <- add_rx_residue(rx_out, fuel_df, i)
-                                         
-                                         # change fire weather value names appropriately
-                                         post_rx[, ':=' (Wind_corrected = Wind_corrected_97,
-                                                         Fm10  = Fm10_97,
-                                                         Fm1000 = Fm1000_97)]
-                                         
-                                         # burn it with wildfire
-                                         output_df <- burn_residue(post_rx, "None")
-                                         
-                                         # save the output
-                                         save_output(output_df,
-                                                     Silvicultural_Treatment,
-                                                     Harvest_System,
-                                                     Harvest_Type,
-                                                     Burn_Type,
-                                                     tile_number,
-                                                     i)
-                                         
-                                 })
-                                 
-                         } else {
-                                 
-                                 # create a vector from 0-100 years
-                                 timestep <- seq(0, 100, 25)
-                                 
-                                 # assign the vector names, otherwise position will be 
-                                 # off by 1 from the value
-                                 names(timestep) <- as.character(timestep)
-                                 
-                                 # calculate the remaining fuel for each timestep and
-                                 # add to the fuelbed
-                                 lapply(timestep, function(i) {
-                                         
-                                         # need to copy dt or it is modified 
-                                         cpy <- copy(fuel_df)
-                                         
-                                         # calculate piled load
-                                         cpy <- pile_residue(cpy, i)
-                                         
-                                         # add the remaining residue to the fuelbed
-                                         cpy <-  add_residue(cpy, i)
-                                         
-                                         # change fire weather value names appropriately
-                                         cpy[, ':=' (Wind_corrected = Wind_corrected_97,
-                                                     Fm10  = Fm10_97,
-                                                     Fm1000 = Fm1000_97)]
-                                         
-                                         # burn it
-                                         output_df <- burn_residue(cpy, Burn_Type)
-                                         
-                                         # save the output
-                                         save_output(output_df,
-                                                     Silvicultural_Treatment,
-                                                     Harvest_System,
-                                                     Harvest_Type,
-                                                     Burn_Type,
-                                                     tile_number,
-                                                     i)
-                                         
-                                 })
-                                 
-                         }
-                         
-                 })
+        future_lapply(scenario_list, 
+                      function(x) {
+                              
+                              ID <- x[1, ID]
+                              Silvicultural_Treatment <- x[1, Silvicultural_Treatment]
+                              Harvest_System <- x[1, Harvest_System]
+                              Harvest_Type <- x[1, Harvest_Type]
+                              Burn_Type <- x[1, Burn_Type]
+                              Biomass_Collection <- x[1, Biomass_Collection]
+                              Tile_Number <- x[1, Tile_Number]
+                              
+                              # load data
+                              # this combines residue, raster, and FCCS data
+                              fuel_df <- load_data(ID,
+                                                   Silvicultural_Treatment,
+                                                   Harvest_System,
+                                                   Harvest_Type, 
+                                                   Burn_Type,
+                                                   Biomass_Collection,
+                                                   Tile_Number)
+                              
+                              # correct windspeed
+                              wind_correction(fuel_df,
+                                              Wind,
+                                              TPA,
+                                              TPI)
+                              
+                              if(Burn_Type != "None") {
+                                      
+                                      # need to copy dt or it is modified 
+                                      cpy <- copy(fuel_df)
+                                      
+                                      # calculate piled load
+                                      cpy <- pile_residue(cpy, 0)
+                                      
+                                      # add the remaining residue to the fuelbed
+                                      cpy <-  add_residue(cpy, 0)
+                                      
+                                      # change fire weather value names appropriately
+                                      cpy[, ':=' (Wind_corrected = Wind_corrected_rx,
+                                                  Fm10  = Fm10_rx,
+                                                  Fm1000 = Fm1000_rx)]
+                                      
+                                      # apply the rx burn
+                                      rx_out <- burn_residue(cpy, Burn_Type)
+                                      
+                                      # save the output
+                                      save_output(rx_out,
+                                                  Silvicultural_Treatment,
+                                                  Harvest_System,
+                                                  Harvest_Type,
+                                                  Burn_Type,
+                                                  tile_number,
+                                                  0)
+                                      
+                                      # create a vector from 25-100 years
+                                      timestep <- seq(25, 100, 25)
+                                      
+                                      # assign the vector names, otherwise position will be 
+                                      # off by 1 from the value
+                                      names(timestep) <- as.character(timestep)
+                                      
+                                      # calculate the remaining fuel for each timestep and
+                                      # add to the fuelbed
+                                      lapply(timestep, function(i) {
+                                              
+                                              # create post RX recovered fuelbed for year i
+                                              post_rx <- add_rx_residue(rx_out, fuel_df, i)
+                                              
+                                              # change fire weather value names appropriately
+                                              post_rx[, ':=' (Wind_corrected = Wind_corrected_97,
+                                                              Fm10  = Fm10_97,
+                                                              Fm1000 = Fm1000_97)]
+                                              
+                                              # burn it with wildfire
+                                              output_df <- burn_residue(post_rx, "None")
+                                              
+                                              # save the output
+                                              save_output(output_df,
+                                                          Silvicultural_Treatment,
+                                                          Harvest_System,
+                                                          Harvest_Type,
+                                                          Burn_Type,
+                                                          tile_number,
+                                                          i)
+                                              
+                                      })
+                                      
+                              } else {
+                                      
+                                      # create a vector from 0-100 years
+                                      timestep <- seq(0, 100, 25)
+                                      
+                                      # assign the vector names, otherwise position will be 
+                                      # off by 1 from the value
+                                      names(timestep) <- as.character(timestep)
+                                      
+                                      # calculate the remaining fuel for each timestep and
+                                      # add to the fuelbed
+                                      lapply(timestep, function(i) {
+                                              
+                                              # need to copy dt or it is modified 
+                                              cpy <- copy(fuel_df)
+                                              
+                                              # calculate piled load
+                                              cpy <- pile_residue(cpy, i)
+                                              
+                                              # add the remaining residue to the fuelbed
+                                              cpy <-  add_residue(cpy, i)
+                                              
+                                              # change fire weather value names appropriately
+                                              cpy[, ':=' (Wind_corrected = Wind_corrected_97,
+                                                          Fm10  = Fm10_97,
+                                                          Fm1000 = Fm1000_97)]
+                                              
+                                              # burn it
+                                              output_df <- burn_residue(cpy, Burn_Type)
+                                              
+                                              # save the output
+                                              save_output(output_df,
+                                                          Silvicultural_Treatment,
+                                                          Harvest_System,
+                                                          Harvest_Type,
+                                                          Burn_Type,
+                                                          tile_number,
+                                                          i)
+                                              
+                                      })
+                                      
+                              }
+                              
+                      })
 }
