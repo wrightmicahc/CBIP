865d8d6998115ca782b1ca1a8029a5b153c630c3
Micah C Wright <mcw11@SERC-S26492.local>
2019-03-26 10:05:06 -0700

    Update documentation and remove cartesian join.

--- scripts/scenarios/load_data.R
+++ scripts/scenarios/load_data.R
@@ -1,9 +1,13 @@
 ################################################################################
-# This script loads the raster and tabular data and combines them into a single
-# data.table as part of the California Biopower Impact Project. 
+# This script loads the residue, fuelbed, and location attribute data and 
+# combines them into a single data.table as part of the California Biopower 
+# Impact Project. 
 # 
-# scenario: character, rx burn scenario. Either "none", "pile", "broadcast", or
-# "jackpot".
+# id: scenario id number
+# harvest_system: Ground, Cable, or None
+# harvest_type: Whole_Tree, Cut_to_length, or None
+# burn_type: None (wf), Broacast, Pile, or Jackpot
+# biomass_collection: yes/no
 # tile_number: numeric tile number.
 #
 # Author: Micah Wright, Humboldt State University
@@ -15,7 +19,7 @@ library(data.table)
 # define function
 load_data <- function(id, treatment, harvest_system, harvest_type, burn_type, biomass_collection, tile_number) {
         
-        # file path to residue tables
+        # file paths to residue tables
         residue_path <- list("No_Action" = "data/UW/residue/NoAction.csv",
                              "Clearcut" = "data/UW/residue/Remove100Percent.csv",
                              "20_Thin_from_Above" = "data/UW/residue/ThinFromAboveRemove20PercentBA.csv",
@@ -55,7 +59,7 @@ load_data <- function(id, treatment, harvest_system, harvest_type, burn_type, bi
         # filter residue data to include only those in the current tile
         residue <- residue[FCID2018 %in% rdf$FCID2018]
         
-        # specify treatment etc.
+        # specify scenario treatments and attributes
         residue[, ':=' (ID = id,
                         Silvicultural_Treatment = treatment, 
                         Harvest_System = harvest_system,
@@ -67,7 +71,7 @@ load_data <- function(id, treatment, harvest_system, harvest_type, burn_type, bi
         # merge tabulated raster data to fuel proportion and residue data 
         rdf <- merge(rdf, fuel_prop, by = "fuelbed_number")
         
-        fuel_df <- merge(rdf, residue, by = "FCID2018", allow.cartesian = TRUE)
+        fuel_df <- merge(rdf, residue, by = "FCID2018", allow.cartesian = FALSE)
         
         # remove data frames no longer needed
         rm(fuel_prop)
