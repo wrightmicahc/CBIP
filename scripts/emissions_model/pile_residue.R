82487d6c09a051ec4b5c32475cb0444c8a2167b2
Micah Wright <wrightmicahc@gmail.com>
2019-04-12 10:40:12 -0700

    Remove calculations for piled in field and change piled at landing to just piled. There is no longer a distinction between the two.

--- scripts/emissions_model/pile_residue.R
+++ scripts/emissions_model/pile_residue.R
@@ -14,13 +14,13 @@ pile_residue <- function(dt, timestep) {
         # specify the coefficient for pile K
         pK_coeff <- 0.7516606
 
-        # load the lookup table for landing piles
-        lookup_landing <- fread("data/SERC/lookup_tables/piled_at_landing.csv", 
+        # load the lookup table for piled fuels
+        lookup_pile <- fread("data/SERC/lookup_tables/piled_fuels.csv", 
                                 verbose = FALSE)
         
         # merge lookup and dt
         dt <-  merge(dt, 
-                     lookup_landing,
+                     lookup_pile,
                      by = c("ID",
                             "Silvicultural_Treatment",
                             "Harvest_System",
@@ -36,70 +36,18 @@ pile_residue <- function(dt, timestep) {
         dt[, CWD := (Stem_ge9_tonsAcre * Stem_ge9) + (Stem_6t9_tonsAcre * Stem_6t9) + (Stem_4t6_tonsAcre * Stem_4t6)]
         
         # calculate landing pile load
-        dt[, pile_landing := decay_fun(CWD,
-                                       CWD_K * pK_coeff,
-                                       timestep) + 
+        dt[, pile_load := decay_fun(CWD,
+                                    CWD_K * pK_coeff,
+                                    timestep) + 
                    to_duff(CWD,
-                                CWD_K * pK_coeff,
-                                timestep) +
+                           CWD_K * pK_coeff,
+                           timestep) +
                    decay_fun(Branch_tonsAcre * Branch,
                              FWD_K * pK_coeff,
                              timestep) +
                    to_duff(Branch_tonsAcre * Branch,
-                                FWD_K * pK_coeff,
-                                timestep) +
-                   decay_foliage(Foliage_tonsAcre * Foliage, 
-                                                Foliage_K * pK_coeff,
-                                                timestep,
-                                                "foliage") +
-                   decay_foliage(Foliage_tonsAcre * Foliage, 
-                                      Foliage_K * pK_coeff, 
-                                      timestep,
-                                      "duff")]
-        
-        # remove excess columns
-        dt[, c("Type",
-               "Stem_ge9",
-               "Stem_6t9",
-               "Stem_4t6",
-               "Branch",
-               "Foliage",
-               "CWD") := NULL]
-        
-        # load the lookup table for landing piles
-        lookup_field <- fread("data/SERC/lookup_tables/piled_in_field.csv", 
-                              verbose = FALSE)
-        
-        # merge lookup and dt
-        dt <-  merge(dt, 
-                     lookup_field,
-                     by = c("ID",
-                            "Silvicultural_Treatment",
-                            "Harvest_System",
-                            "Harvest_Type",
-                            "Burn_Type",
-                            "Biomass_Collection"), 
-                     all.x = TRUE,
-                     all.y = FALSE,
-                     sort = TRUE,
-                     allow.cartesian = FALSE)
-        
-        # calculate coarse load
-        dt[, CWD := (Stem_ge9_tonsAcre * Stem_ge9) + (Stem_6t9_tonsAcre * Stem_6t9) + (Stem_4t6_tonsAcre * Stem_4t6)]
-        
-        # calculate field pile load
-        dt[, pile_field := decay_fun(CWD,
-                                       CWD_K * pK_coeff,
-                                       timestep) + 
-                   to_duff(CWD,
-                                CWD_K * pK_coeff,
-                                timestep) +
-                   decay_fun(Branch_tonsAcre * Branch,
-                             FWD_K * pK_coeff,
-                             timestep) +
-                   to_duff(Branch_tonsAcre * Branch,
-                                FWD_K * pK_coeff,
-                                timestep) +
+                           FWD_K * pK_coeff,
+                           timestep) +
                    decay_foliage(Foliage_tonsAcre * Foliage, 
                                  Foliage_K * pK_coeff,
                                  timestep,
