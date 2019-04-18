8177479c1ad03e52be22f180d34d1aa01914f8c9
Micah Wright <19318898+wrightmicahc@users.noreply.github.com>
2019-04-17 20:17:05 -0700

    Add conditional that updates column based on burn tyoe.

--- scripts/emissions_model/remove_rx_consumed.R
+++ scripts/emissions_model/remove_rx_consumed.R
@@ -9,23 +9,57 @@
 ################################################################################
 
 
-remove_rx_consumed <- function(dt) {
+remove_rx_consumed <- function(dt, burn_type) {
         
         # copy 
         pdt <- copy(dt)
         
-        # caclulate remaining fuel for each size class
-        pdt[, ':=' (duff_upper_loading = duff_upper_loading - total_duff,
-                    litter_loading = litter_loading - total_litter,
-                    one_hr_sound = 0,
-                    ten_hr_sound = 0,
-                    hun_hr_sound = hun_hr_sound - total_100,
-                    oneK_hr_sound = oneK_hr_sound - total_OneK_snd,
-                    oneK_hr_rotten = oneK_hr_rotten - total_OneK_rot,
-                    tenK_hr_sound = tenK_hr_sound - total_tenK_snd,
-                    tenK_hr_rotten = tenK_hr_rotten - total_tenK_rot,
-                    tnkp_hr_sound = tnkp_hr_sound - total_tnkp_snd,
-                    tnkp_hr_rotten = tnkp_hr_rotten - total_tnkp_rot)]
+        if (burn_type == "Pile") {
+                
+                pdt[, pile_load := 0]
+                
+        } else {
+                
+                # caclulate remaining fuel for each size class
+                pdt[, ':=' (duff_upper_loading = duff_upper_loading - total_duff,
+                            litter_loading = litter_loading - total_litter,
+                            one_hr_sound = 0,
+                            ten_hr_sound = 0,
+                            hun_hr_sound = hun_hr_sound - total_100,
+                            oneK_hr_sound = oneK_hr_sound - total_OneK_snd,
+                            oneK_hr_rotten = oneK_hr_rotten - total_OneK_rot,
+                            tenK_hr_sound = tenK_hr_sound - total_tenK_snd,
+                            tenK_hr_rotten = tenK_hr_rotten - total_tenK_rot,
+                            tnkp_hr_sound = tnkp_hr_sound - total_tnkp_snd,
+                            tnkp_hr_rotten = tnkp_hr_rotten - total_tnkp_rot)]
+                
+        }
+        
+        # remove unecessary columns
+        pdt[, c("total_duff", 
+                "total_litter",
+                "total_1",
+                "total_10",
+                "total_100",
+                "total_OneK_snd",
+                "total_OneK_rot",
+                "total_tenK_snd", 
+                "total_tenK_rot",
+                "total_tnkp_snd", 
+                "total_tnkp_rot",
+                "flamg_pile", 
+                "smoldg_pile",
+                "resid_pile", 
+                "char_100",
+                "char_OneK_snd",
+                "char_OneK_rot",
+                "char_tenK_snd",
+                "char_tenK_rot",
+                "char_tnkp_snd",
+                "char_tnkp_rot",
+                "char_fwd_residue",
+                "char_cwd_residue",
+                "pile_char") := NULL]
         
         return(pdt)
 }
