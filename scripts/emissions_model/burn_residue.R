1e424ebeb9689df4d2d02b2f616dc3d0638294a6
Micah Wright <{19318898}+{wrightmicahc}@users.noreply.github.com>
2019-05-08 21:00:14 -0700

    Update documentation and fix error with multiple := in a call.

--- scripts/emissions_model/burn_residue.R
+++ scripts/emissions_model/burn_residue.R
@@ -15,17 +15,21 @@ source("scripts/Consume/calc_emissions.R")
 source("scripts/emissions_model/remove_rx_consumed.R")
 
 burn_residue <- function(dt, burn_type) {
+        
         # wildfire
         if(burn_type  == "None") {
                 
                 # specify diameter reduction factor
                 DRR  <- 2.0 / 3.0
+                
+                # simulate wildfire
                 consumption_df <- ccon_activity_fast(dt, 
                                                      fm_type = "NFDRS_Th",
                                                      days_since_rain = 50,
                                                      DRR = DRR,
                                                      burn_type = burn_type)
                 
+                # calculate emissions and residual fuels
                 emissions_df <- calc_emissions(consumption_df, burn_type)
                 
         }
@@ -34,27 +38,33 @@ burn_residue <- function(dt, burn_type) {
                 
                 # specify diameter reduction factor
                 DRR  <- 1 
+                
+                # simulate RX
                 consumption_df <- ccon_activity_fast(dt, 
                                                      fm_type = "NFDRS_Th", 
                                                      days_since_rain = 10,
                                                      DRR = DRR, 
                                                      burn_type = burn_type)
                 
+                # remove consumed fuel
                 burn_again <- remove_rx_consumed(consumption_df, burn_type)
                 
                 # update fire weather
-                burn_again[, Fm10  := Fm10_97,
-                           Fm1000 := Fm1000_97,
-                           Wind_corrected := Wind_corrected_97]
+                burn_again[,":=" (Fm10  = Fm10_97,
+                                  Fm1000 = Fm1000_97,
+                                  Wind_corrected = Wind_corrected_97)]
                 
+                # simulate wildfire
                 consumption_df2 <- ccon_activity_fast(burn_again, 
                                                       fm_type = "NFDRS_Th", 
                                                       days_since_rain = 50,
                                                       DRR = 2.0 / 3.0, 
                                                       burn_type = "None")
                 
+                # calculate emissions and residual fuels for RX
                 emissions_df <- calc_emissions(consumption_df, burn_type)
                 
+                # calculate emissions and residual fuels for wildfire
                 emissions_df2 <- calc_emissions(consumption_df2, "None")
                 
         }
@@ -62,23 +72,28 @@ burn_residue <- function(dt, burn_type) {
         # RX piled only
         if(burn_type  == "Pile") {
                 
+                # simulate pile burn
                 consumption_df <- ccon_activity_piled_only_fast(dt)
                 
+                # remove consumed fuel
                 burn_again <- remove_rx_consumed(consumption_df, burn_type)
                 
                 # update fire weather
-                burn_again[,Fm10  := Fm10_97,
-                           Fm1000 := Fm1000_97,
-                           Wind_corrected := Wind_corrected_97]
+                burn_again[,":=" (Fm10  = Fm10_97,
+                                  Fm1000 = Fm1000_97,
+                                  Wind_corrected = Wind_corrected_97)]
                 
+                # simulate wildfire
                 consumption_df2 <- ccon_activity_fast(burn_again, 
                                                       fm_type = "NFDRS_Th", 
                                                       days_since_rain = 50,
                                                       DRR = 2.0 / 3.0, 
                                                       burn_type = "None")
                 
+                # calculate emissions and residual fuels for RX
                 emissions_df <- calc_emissions(consumption_df, burn_type)
                 
+                # calculate emissions and residual fuels for wildfire
                 emissions_df2 <- calc_emissions(consumption_df2, "None")
                 
         }
